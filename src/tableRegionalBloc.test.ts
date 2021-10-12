72c93ad4ce24f55c4ce4fab88acdb2a97c1e8648
rafdul <rafalduluk@gmail.com>
2021-10-12 10:51:48 +0200

    Add test for aggregate of population

--- src/tableRegionalBloc.test.ts
+++ src/tableRegionalBloc.test.ts
@@ -14,11 +14,21 @@ describe('Check, if new object with regional blocs is created and:' , () => {
 
     test(`check, if countries' currencies are matched with regional blocs`, () => {
         let testRegBloc = startRegionalbloc(mockValues.statesForNewObj);
-        console.log('==test2==', testRegBloc)
+        // console.log('==test2==', testRegBloc)
 
         expect(testRegBloc.EU.currencies.length).toEqual(2);
         expect(testRegBloc.EU.currencies.some(el => el === 'CZK')).toBe(true);
         expect(testRegBloc.other.currencies.length).toEqual(1);
         expect(testRegBloc.other.currencies.some(el => el === 'CNY')).toBe(true);
-    })
+    });
+
+    test(`check, if countries' populations are summed`, () => {
+        let testRegBloc = startRegionalbloc(mockValues.statesForNewObj);
+        console.log('==test3==', testRegBloc);
+        let populationForNafta = 132322035;
+        let populationForOther = 1153261217;
+
+        expect(testRegBloc.NAFTA.population).toEqual(populationForNafta);
+        expect(testRegBloc.other.population).toEqual(populationForOther);
+    });
 });
